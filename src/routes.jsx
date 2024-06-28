fbefd5b09e480e74a18e3ef3f1e3097815d4df2b
hardy07 <coppersakthi@gmail.com>
2024-06-28 22:22:58 +0530

    updated routes.jsx

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,6 +1,6 @@
 import { createBrowserRouter } from "react-router-dom";
 import React, { lazy, Suspense } from "react";
-import RedirectAnd404 from "./pages/RedirectAnd404";
+import RedirectAnd404 from "./pages/redirectand404.jsx";
 const Home = lazy(() => import("./pages/Home"));
 const Contact = lazy(() => import("./pages/Contact"));
 
