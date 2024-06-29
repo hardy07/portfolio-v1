1af1477ec2ece45cb827b8a3afaff3c4609ff68e
hardy07 <coppersakthi@gmail.com>
2024-06-30 01:09:08 +0530

    update3

--- constants.js
+++
@@ -1,2 +0,0 @@
-export const github_username = "hardy07";
-export const github_url = "hardy07";
--- src/routes.jsx
+++ src/routes.jsx
@@ -1,8 +1,8 @@
 import { createBrowserRouter } from "react-router-dom";
 import React, { lazy, Suspense } from "react";
 import RedirectAnd404 from "./pages/redirectand404.jsx";
-const Home = lazy(() => import("./pages/home.jsx"));
-const Contact = lazy(() => import("./pages/contact.jsx"));
+const Home = lazy(() => import("./pages/Home.jsx"));
+const Contact = lazy(() => import("./pages/Contact.jsx"));
 
 const router = createBrowserRouter([
   {
