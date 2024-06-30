349758091a435bf2e824a6373013d5b59f7b1849
hardy07 <coppersakthi@gmail.com>
2024-06-30 15:37:59 +0530

    routes fix1

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,22 +1,20 @@
-import { createBrowserRouter } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import React, { lazy, Suspense } from "react";
 import RedirectAnd404 from "./pages/redirectand404.jsx";
+
 const Home = lazy(() => import("./pages/home.jsx"));
 const Contact = lazy(() => import("./pages/contact.jsx"));
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Home />,
-  },
-  {
-    path: "/contact",
-    element: <Contact />,
-  },
-  {
-    path: "*",
-    element: <RedirectAnd404 />,
-  },
-]);
+const Routes = () => (
+  <Router>
+    <Suspense fallback={<div>Loading...</div>}>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="*" element={<RedirectAnd404 />} />
+      </Routes>
+    </Suspense>
+  </Router>
+);
 
-export default router;
+export default Routes;
