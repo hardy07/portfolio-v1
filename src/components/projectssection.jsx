14f1e984b969a6cf589021484b174d81ea23d86b
hardy07 <coppersakthi@gmail.com>
2024-07-05 01:45:33 +0530

    Image update

--- src/components/projectssection.jsx
+++ src/components/projectssection.jsx
@@ -6,7 +6,7 @@ const projects = [
     name: "Radio Javan Downloader",
     type: "Web Application",
     images: [
-      "https://media.discordapp.net/attachments/1256687941711826985/1256689099784589312/prj1.png?ex=6681ae6d&is=66805ced&hm=dd936083c19efb176326aef00db23f96a432230416991f72e93477a3fe7956cc&=&format=webp&quality=lossless&width=1250&height=592",
+      "https://media.discordapp.net/attachments/1256687941711826985/1258516093601841205/screencapture-rj-hoseinwave-ir-2024-01-26-15-34-26.png?ex=668853f2&is=66870272&hm=ce26f3c3c3d79153a202abc7961d5d3c0351f3b755c7c61e023d2d9f5cd077fa&=&format=webp&quality=lossless&width=1250&height=592",
     ],
     description:
       "Web Application for downloading any kind of media from radiojavan.com website",
@@ -30,7 +30,7 @@ const projects = [
     name: "Weather App",
     type: "Web Application",
     images: [
-      "https://media.discordapp.net/attachments/1256687941711826985/1256689486352486410/prj2.png?ex=6681aec9&is=66805d49&hm=ca00ed4e645445cbb0c85697fbab27b05e534cd77e81eea7a6f935aacfbaf7b5&=&format=webp&quality=lossless&width=1177&height=593",
+      "https://media.discordapp.net/attachments/1256687941711826985/1258516093983527062/screencapture-weather-projects-hoseinwave-ir-2024-01-29-22-35-11.png?ex=668853f2&is=66870272&hm=4bfc0b0345bcf120ad126245272883022eb7043fd41ca3bea3c799aa43a04331&=&format=webp&quality=lossless&width=1177&height=593",
     ],
     description:
       "A Web Application that shows current and forecast weather information based on I",
@@ -53,7 +53,7 @@ const projects = [
     name: "Persian TTS",
     type: "Web Application",
     images: [
-      "https://media.discordapp.net/attachments/1256687941711826985/1256689601829929020/prj3.png?ex=6681aee5&is=66805d65&hm=d5c9a66e70c526162255fe84b589105c608af8f3b593dd3e6277d6383e0b6414&=&format=webp&quality=lossless&width=1175&height=593",
+      "https://media.discordapp.net/attachments/1256687941711826985/1258516094432579664/image.png?ex=668853f3&is=66870273&hm=5d15a6499d251d35cd01643238c09fc8d47c90dfad166caedfaac84326b70ce7&=&format=webp&quality=lossless&width=1175&height=593",
     ],
     description:
       "A web app that converts Farsi text to speech with igap messenger api.",
